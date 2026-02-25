8f686fbe5be2dd0d8a3301ac31877f135fe884a1
Ray Vega <iosifstanevich0@gmail.com>
2026-02-25 17:48:45 +0300

    index.js

--- index.js
+++ index.js
@@ -7,31 +7,25 @@ app.get('/', (req, res) => res.send('Elysium Bot is Online!'));
 app.listen(process.env.PORT || 3000);
 
 const bot = mineflayer.createBot({
-  host: 'Elysium-62TP.aternos.me', // ВПИШИ СВОЙ АДРЕС ТУТ
-  port: 44193,
-  username: 'Elysium-62TP',
+  host: 'ТВОЙ_IP.aternos.me', // Максим, тут впиши свой адрес еще раз
+  port: 19132,
+  username: 'Elysium_Guard',
   offline: true,
-  version: '1.21.131.1'
+  version: '1.21.1' // Оставляем именно так, это "общая" версия для 1.21
 });
 
 bot.on('spawn', () => {
-  console.log('Максим, бот зашел!');
+  console.log('Максим, я зашел на сервер!');
   setInterval(() => {
-    const actions = ['jump', 'sneak'];
-    const act = actions[Math.floor(Math.random() * actions.length)];
-    bot.setControlState(act, true);
-    setTimeout(() => bot.setControlState(act, false), 1000);
+    bot.setControlState('jump', true);
+    setTimeout(() => bot.setControlState('jump', false), 500);
     bot.look(Math.random() * 360, 0);
   }, 60000); 
 });
 
-bot.on('chat', (username, message) => {
-  if (message.toLowerCase().includes('привет')) {
-    bot.chat('Привет! Я охранник Elysium.');
-  }
-});
+bot.on('error', (err) => console.log('Ошибка:', err));
 
 bot.on('end', () => {
   console.log('Вылет! Перезапуск через 1 минуту...');
-  setTimeout(() => process.exit(1), 60000); // Render сам перезапустит процесс
+  setTimeout(() => process.exit(1), 60000); 
 });
