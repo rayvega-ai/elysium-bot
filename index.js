d694f2c7f8f51ddf6c9d0d6ad99089e820da9611
Ray Vega <iosifstanevich0@gmail.com>
2026-02-25 18:06:48 +0300

    index.js

--- index.js
+++ index.js
@@ -2,30 +2,36 @@ const mineflayer = require('mineflayer');
 const express = require('express');
 const app = express();
 
-// Веб-сервер для Render
-app.get('/', (req, res) => res.send('Elysium Bot is Online!'));
+app.get('/', (req, res) => res.send('Elysium Bot Status: OK'));
 app.listen(process.env.PORT || 3000);
 
+console.log('Попытка подключения к Elysium...');
+
 const bot = mineflayer.createBot({
-  host: 'ТВОЙ_IP.aternos.me', // Максим, тут впиши свой адрес еще раз
-  port: 44193,
+  host: 'ТВОЙ_IP.aternos.me', // ПРОВЕРЬ ЕЩЕ РАЗ!
+  port: 44193, // Твой последний порт из логов
   username: 'Elysium_Guard',
   offline: true,
-  version: '1.21.1' // Оставляем именно так, это "общая" версия для 1.21
+  version: false // Бот сам подберет версию протокола
+});
+
+bot.on('login', () => {
+  console.log('Бот успешно вошел в аккаунт!');
 });
 
 bot.on('spawn', () => {
-  console.log('Максим, я зашел на сервер!');
-  setInterval(() => {
-    bot.setControlState('jump', true);
-    setTimeout(() => bot.setControlState('jump', false), 500);
-    bot.look(Math.random() * 360, 0);
-  }, 60000); 
+  console.log('Максим, я заспавнился в мире!');
 });
 
-bot.on('error', (err) => console.log('Ошибка:', err));
+bot.on('error', (err) => {
+  console.log('ОШИБКА ПОДКЛЮЧЕНИЯ:', err.message);
+});
+
+bot.on('kicked', (reason) => {
+  console.log('Бот был КИКНУТ сервером:', reason);
+});
 
 bot.on('end', () => {
-  console.log('Вылет! Перезапуск через 1 минуту...');
-  setTimeout(() => process.exit(1), 60000); 
+  console.log('Соединение закрыто. Перезапуск через 30 сек...');
+  setTimeout(() => process.exit(1), 30000);
 });
